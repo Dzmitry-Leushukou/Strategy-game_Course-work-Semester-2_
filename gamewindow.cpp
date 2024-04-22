f12ccd8a883a59dcc0c1897c772679eb04d85027
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-04-22 21:46:15 +0300

    Check

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -6,18 +6,10 @@ GameWindow::GameWindow(QWidget *parent) :
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
-    //QScreen *chosen = this;
-    QRect resolution=this->geometry();
-    scene.setSceneRect(0, 0, resolution.width(), resolution.height());
-    View *view = new View(&scene);
-
-    //showFullScreen();
-    //QTimer::singleShot(1000, this, SLOT(showFullScreen()));
-
-    // /this->setCentralWidget(view);
 }
 
 GameWindow::~GameWindow()
 {
     delete ui;
 }
+//Civ
