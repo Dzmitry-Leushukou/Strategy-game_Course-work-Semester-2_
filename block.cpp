2a4033fc1da1228b7040f3567b06c6fe63423294
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-04-22 14:27:35 +0300

    Block implemented

--- block.cpp
+++ block.cpp
@@ -1,3 +1,34 @@
 #include "block.h"
 
-Block::Block() {}
+//Block::Block() {}
+
+Block::Block(int x, int y, int id)
+{
+    this->x=x;
+    this->y=y;
+    this->id=id;
+    this->texture=getTextureByID(id);
+}
+
+void Block::update()
+{
+
+}
+
+int Block::getRX()
+{
+    return this->x*SIZE;
+}
+
+int Block::getRY()
+{
+    return this->y*SIZE;
+}
+
+QRectF Block::getTextureByID(int)
+{
+    int tileSize = 16, tilesOnLine = 16;
+    float tx = id%tilesOnLine, ty = id/tilesOnLine;
+    QRectF texture = QRectF(tx * tileSize, ty * tileSize, tileSize, tileSize);
+    return texture;
+}
--- block.h
+++ block.h
@@ -1,10 +1,22 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+#include <QRectF>
+
 class Block
 {
 public:
-    Block();
+    Block(int, int, int);
+    void update();
+    //Real coords
+    int getRX();
+    int getRY();
+
+    QRectF getTextureByID(int);
+    QRectF texture;
+
+    const static int SIZE = 32;
+    int id,x,y;
 };
 
 #endif // BLOCK_H
