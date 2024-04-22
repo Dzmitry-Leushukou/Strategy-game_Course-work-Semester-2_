f1a2e9d63b74fb3a5af02323dae8c28eab8d4eb9
Dzmitry Leushukou <dmitrylevshukov@outlook.com>
2024-04-22 15:51:20 +0300

    Implemented camera

--- camera.h
+++ camera.h
@@ -1,10 +1,25 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <QPointF>
+
+class Game;
+
 class Camera
 {
 public:
-    Camera();
+    Camera(Game *game);
+    Game *game;
+    QPointF pos, mouse;
+    bool clickLeft=false,clickRight=false;
+    int mouseBlockX=0,mouseBlockY=0;
+
+    void update();
+    QPointF getPosition();
+    void setPosition(float x, float y);
+    void setPosition(QPointF position);
+    void move(float x, float y);
+    void move(QPointF move);
 };
 
 #endif // CAMERA_H
